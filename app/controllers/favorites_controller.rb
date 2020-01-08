5c0e4949ab43c6c0e60de948327d3947caf400f7
Hiroyuki-uno-0711 <do.for.me.affiliate@gmail.com>
2020-01-08 15:01:50 +0000

    favorite controller complete

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
+    job_information = JobInformation.find(params[:job_information_id])
+    favorite = current_user.favorites.new(job_information_id: job_information.id)
+    favorite.save
+    redirect_to request.referrer
   end
 
   def destroy
+    job_information = JobInformation.find(params[:job_information_id])
+    favorite = current_user.favorites.new(job_information_id: job_information.id)
+    favorite.destroy
+    redirect_to request.referrer
   end
 
 end
