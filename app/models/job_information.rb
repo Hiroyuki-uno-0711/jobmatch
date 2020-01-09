9c36eccfe95695393bd5f2ecc059b1dadeff209e
Hiroyuki-uno-0711 <do.for.me.affiliate@gmail.com>
2020-01-09 06:19:48 +0000

    user show job_information creat_at change

--- app/models/job_information.rb
+++ app/models/job_information.rb
@@ -9,7 +9,7 @@ class JobInformation < ApplicationRecord
   attachment :job_image
 
   # enum関連
-  enum genre: {有形営業: 1, 無形営業: 2, 商品サービス企画: 3, 物流倉庫管理: 4, 一般事務: 5, 人事: 6, 経理: 7, 総務: 8, システムエンジニア: 9, 販売サービス: 10}
+  enum genre: {有形営業: 1, 無形営業: 2, 商品サービス企画: 3, 物流倉庫管理: 4, 一般事務: 5, 人事: 6, 経理: 7, 総務: 8, システムエンジニア: 9, その他サービス: 10}
   enum area: {東京: 1, 神奈川: 2, 千葉: 3, 埼玉: 4}
 
   # イイネ機能関連
@@ -19,11 +19,11 @@ class JobInformation < ApplicationRecord
 
   def create_notification_like!(current_user)
     # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and book_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and job_information_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
     # いいねされていない場合のみ、通知レコードを作成
     if temp.blank?
       notification = current_user.active_notifications.new(
-        book_id: id,
+        job_information_id: id,
         visited_id: user_id,
         action: 'like'
       )
